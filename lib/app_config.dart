5b3ca7b51a424a8311150914de12c5498962e9af
Mohammed Riyad <riyadm2001@gmail.com>
2025-03-19 15:08:31 +0200

    fix domain

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -35,7 +35,7 @@ class AppConfig {
   static const bool HTTPS =
       true; //if you are using localhost , set this to false
   static const DOMAIN_PATH =
-      "devefinance.com"; //use only domain name without http:// or https://
+      "appleerastore.com"; //use only domain name without http:// or https://
 
   //do not configure these below
   static const String API_ENDPATH = "api/v2";
